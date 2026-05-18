d1bfaff864d86fa828782329aa54518d61f50a8f
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-18 17:55:38 +0200

    ADD handle per serie ma forse è meglio se lo tolgo e unifico con if

--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { createContext } from "react";
-import { searchMovie } from "../hooks/useFetch";
+import { searchMovie, searchTvShow } from "../hooks/useFetch";
 import { useEffect } from "react";
 
 
@@ -8,24 +8,47 @@ const SearchContext = createContext(null);
 
 function SearchProvider({ children }) {
     const [movies, setMovies] = useState([]);
+    const [shows, setShows] = useState([]);
+    
     const [errorMsg, setErrorMsg] = useState('');
 
-
+    // ================================
+    // Handle per film (da fare DRY)
+    // ================================
     const handleSearch = ((query) => {
         searchMovie(query)
             .then(data => {
                 setMovies(data)
                 console.log(data);
-                
+
             })
             .catch(error => {
-                if (error.message === 'Pagina non trovata'){
+                if (error.message === 'Pagina non trovata') {
                     setErrorMsg(error.message)
                 } else {
                     setErrorMsg('Qualcosa è andato storto')
                 }
+            });
     });
-});
+
+    // ================================
+    // Handle per serie (da fare DRY)
+    // ================================
+    const handleSearchTv = ((query) => {
+        searchTvShow(query)
+            .then(data => {
+                setShows(data)
+                console.log(data);
+
+            })
+            .catch(error => {
+                if (error.message === 'Pagina non trovata') {
+                    setErrorMsg(error.message)
+                } else {
+                    setErrorMsg('Qualcosa è andato storto')
+                }
+            });
+    });
 
 
     const value = {
@@ -44,4 +67,4 @@ function SearchProvider({ children }) {
 
 };
 
-export {SearchContext, SearchProvider}
+export { SearchContext, SearchProvider }
