7d6656e6cec8f2bb72e5ff76d8c237759f31eb5d
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-05-19 17:24:44 +0200

    ADD aggiunti trending alle rispettive pagine

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,13 +1,16 @@
 import { useContext } from "react";
 import { SearchContext } from "../contexts/SearchContext";
 import MediaList from "../components/MediaList";
+import { TrendContext } from "../contexts/TrendingContext";
+import TrendList from "../components/TrendList";
 
 
 function Movies() {
-  const { movies } = useContext(SearchContext)
-
+  const { movies } = useContext(SearchContext);
+  const {trendingMovies} = useContext(TrendContext);
 
   return <>
+    <TrendList title='Trending Movies' items={trendingMovies} type='movie' />
     <MediaList title='TV Shows' items={movies} type='movie' />
   </>
 }
--- src/pages/TvShows.jsx
+++ src/pages/TvShows.jsx
@@ -1,12 +1,16 @@
 import { useContext } from "react";
 import { SearchContext } from "../contexts/SearchContext";
 import MediaList from "../components/MediaList";
+import { TrendContext } from "../contexts/TrendingContext";
+import TrendList from "../components/TrendList";
 
 
 function TvShows() {
-  const {shows} = useContext(SearchContext)
+  const {shows} = useContext(SearchContext);
+  const {trendingShows} = useContext(TrendContext);
 
   return <>
+    <TrendList title='Trending Shows' items={trendingShows} type='show' />
     <MediaList title='TV Shows' items={shows} type='show' />
   </>
 }
